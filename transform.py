092565df3cf11f9e0974705bee812e0f86e06fb0
KhaledAchech <achechkhaled@gmail.com>
2024-08-15 15:38:47 +0200

    gitignore and transform

--- transform.py
+++ transform.py
@@ -18,13 +18,13 @@ def four_point_transform(image, pts):
     rect = order_points(pts)
     (tl, tr, br, bl) = rect
 
-    widthA = np.sqrt(((br[0]-bl[0]**2)+(br[1] - bl[1]**2)))
-    widthB = np.sqrt(((tr[0]-tl[0]**2)+(tr[1] - tl[1]**2)))
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
     maxWidth = max(int(widthA), int(widthB))
 
-    hightA = np.sqrt(((tr[0]-br[0]**2)+(tr[1]-br[1]**2)))
-    hightB = np.sqrt(((tl[0]-bl[0]**2)+(tl[1]-bl[1]**2)))
-    maxHight = max(int(hightA), int(hightB))
+    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    maxHight = max(int(heightA), int(heightB))
 
     dst = np.array([
         [0, 0],
